23c6ed4e73c7a29bac39af4ad99a6b0535c96d02
Ádám István Szűcs <szucs.adamistvan.89@gmail.com>
2018-03-13 13:40:43 +0100

    [Update] Added Course_5 material

---
+++ Course_5/src/main/java/CalculatorService.java
@@ -0,0 +1,6 @@
+public interface CalculatorService {
+    public double add(double aLeft, double aRigh);
+    public double subtract(double aLeft, double aRigh);
+    public double multiply(double aLeft, double aRigh);
+    public double divide(double aLeft, double aRigh);
+ }
---
+++ Course_5/src/main/java/MathApplication.java
@@ -0,0 +1,24 @@
+public class MathApplication {
+    private CalculatorService mCalcService;
+ 
+    public void setCalculatorService(CalculatorService aCalcService){
+       this.mCalcService = aCalcService;
+    }
+    
+    public double add(double aLeft, double aRight){
+       //return mCalcService.add(aLeft, aRight);
+       return aLeft + aRight;
+    }
+    
+    public double subtract(double aLeft, double aRight){
+       return mCalcService.subtract(aLeft, aRight);
+    }
+    
+    public double multiply(double aLeft, double aRight){
+       return mCalcService.multiply(aLeft, aRight);
+    }
+    
+    public double divide(double aLeft, double aRight){
+       return mCalcService.divide(aLeft, aRight);
+    }
+ }
