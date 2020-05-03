582943aebd52ab6079496ec046691848e36bbd58
Adam Szucs <adam.szucs@me.com>
2020-05-03 17:24:07 +0200

    Updated assignment

---
+++ Assigment/src/test/java/MathApplicationTester.java
@@ -0,0 +1,36 @@
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+
+public class MathApplicationTester {
+
+  @Test
+  public void testAdd() {}
+
+  @Test
+  public void testSubtract() {}
+
+  @Test
+  public void testMultiple() {}
+
+  @Test
+  public void testDivide() {}
+
+  @Test
+  public void testDivideWithParameter() {}
+
+  @Test
+  public void testAdditionDifferent() {}
+
+  @Test
+  public void testRaiseRuntimeException() {}
+
+  @Test
+  public void testMockCalculatorService() {}
+}
--- Course_6/src/main/java/com/elte/issuetracker/repositories/IssueRepository.java
+++
@@ -1,8 +0,0 @@
-package com.elte.issuetracker.repositories;
-
-import com.elte.issuetracker.entities.Issue;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IssueRepository extends CrudRepository<Issue, Integer> {
-
-}
